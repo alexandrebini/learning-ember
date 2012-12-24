df3a19578418615ece07528afa33d215832bb5a3
Alexandre Bini <alexandrebini@gmail.com>
2012-12-24 10:56:03 -0200

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photoblog::Application.config.secret_token = '6e0c259129b4fce7285dc632e3855cb8cb4bbb7b9d11d5be38578f7f9adb803e396f5aeaa7458e28f29fe10e83fece0de079505952b222629900d9d7e24d7b60'
